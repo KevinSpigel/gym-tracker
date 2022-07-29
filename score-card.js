01a9a036c4023c2160d307f1e5363ade05da7d44
KevinSpigel <kevinspigel@gmail.com>
2022-07-29 15:50:59 +0200

    Adding LogIn simulator example and constructor method example

--- score-card.js
+++ score-card.js
@@ -1,8 +1,32 @@
 
 // debugger
 
-let shutDown = confirm ("Do you want to Log Out?")
+let shutDown = confirm("Do you want to Log Out?")
 
 if (shutDown === true) {
-    console.log ("See you later💪🏼")
+    console.log("See you later💪🏼")
 }
+
+// debugger
+
+class DayExercise {
+    constructor(exercise, roundRep, weight, time) {
+        this.exercise = exercise;
+        this.roundRep = roundRep;
+        this.weight = weight;
+        this.time = time;
+    }
+}
+
+function newExercise() {
+
+    let exercise = prompt("Exercise");
+    let roundRep = prompt("Rounds, Reps");
+    let weight = prompt("Weight");
+    let time = prompt("Time, Rounds, Reps");
+    const newExercise = new DayExercise(ejercicio, roundRep, weight, time);
+    console.log(newExercise);
+}
+
+
+// Need to add Array function to enable to save new DayExercise.
