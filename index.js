ea26393fed73f60c9c008e8a681e0c417bdc974a
KevinSpigel <kevinspigel@gmail.com>
2022-08-08 12:06:02 +0200

    Fix logIn function

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 
 const userDataBase = [{ user: 'Kev', pwd: '12345' }, { user: 'Gonza', pwd: '22222' }, { user: 'Fer', pwd: '0000' }]
 
-const maxAttemps = []
-
+let attemps = 0
 
 function logIn(user, pwd) {
 
-    debugger
+    // debugger
+
+    if (attemps >= 3) {
+        return alert(" Access Denied - User Blocked: You exceeded the maximum attempts. Contact the administrator.")
+    }
 
     user = prompt("Enter your Username:")
     pwd = prompt("Enter your Password:")
 
-    checkBlockedUser();
 
-    userDataBase.forEach ((element, i) => {
-        if (element.user === user && element.pwd === pwd) {
-            console.log("Welcome", user)
-        }
+    let userProfile = userDataBase.find((element) => {
+        return element.user === user && element.pwd === pwd;
+    });
 
-        else {
-            alert("⛔️ Incorrect Username or Password.")
-            maxAttemps.push(user)
-        }
-    })
-
-
-    function checkBlockedUser() {
-        const blockedUsers = maxAttemps.reduce((accumulator) => accumulator + 0,0)
-        if (blockedUsers >= 3){
-            alert("Blocked User")
-        }
+    if (userProfile) {
+        console.log("Welcome", user)
     }
 
+    else if (userProfile === undefined) {
+        alert("⛔️ Incorrect Username or Password.");
+        attemps++;
+        logIn();
+    }
 }
-
-logIn();
