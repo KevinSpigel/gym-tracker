477e4ded22e2cf36ce7a99975396c473beabeff7
KevinSpigel <kevinspigel@gmail.com>
2022-08-15 19:34:52 +0200

    add DOM to the project // fix score-card and index functions with events elements, buttons functionalities// Add Chronometer

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 
-const userDataBase = [{ user: 'Kev', pwd: '12345' }, { user: 'Gonza', pwd: '22222' }, { user: 'Fer', pwd: '0000' }]
+let nameInput = document.querySelector ("#name");
 
-let attemps = 0
-
-function logIn(user, pwd) {
-
-    // debugger
-
-    if (attemps >= 3) {
-        return alert(" Access Denied - User Blocked: You exceeded the maximum attempts. Contact the administrator.")
-    }
-
-    user = prompt("Enter your Username:")
-    pwd = prompt("Enter your Password:")
+let passInput = document.querySelector ("#pass");
 
 
-    let userProfile = userDataBase.find((element) => {
-        return element.user === user && element.pwd === pwd;
-    });
+const accessGranted = document.querySelector("button");
+accessGranted.addEventListener ("click", userValidation);
+
+
+const userDataBase = [
+    { user: 'Kev', pwd: '12345' }, 
+    { user: 'Gonza', pwd: '22222' }, 
+    { user: 'Fer', pwd: '0000' }
+];
+
+
+
+function userValidation (e){
+    e.preventDefault();
+;
+
+let user = nameInput.value;
+let pwd = passInput.value;
+
+
+let userProfile = userDataBase.find((element) => {
+    return element.user === user && element.pwd === pwd;
+});
 
     if (userProfile) {
-        console.log("Welcome", user)
+        location.href = "score-card.html";
     }
 
     else if (userProfile === undefined) {
-        alert("⛔️ Incorrect Username or Password.");
-        attemps++;
-        logIn();
+
+    let messageDiv = document.createElement ("div");
+    messageDiv.innerHTML = `<h2 class= "modal-dialog alert alert-danger fixed-bottom">⛔️ ACCESS DENIED: Incorrect Username or Password.</h2>`;
+    document.body.append (messageDiv);
+    setTimeout (() => {
+        messageDiv.remove ();
+    }, 2300);
     }
 }
-
-logIn();
--- score-card.js
+++ score-card.js
@@ -1,47 +1,229 @@
 
-// debugger
+// Shut down button
 
-let shutDown = confirm("Do you want to Log Out?")
+const shutDown = document.querySelector("li")
+shutDown.addEventListener("click", logOut)
 
-if (shutDown === true) {
-    alert("See you later💪🏼")
-}
-
-
-
-// This array and function would be use for the selector of "type of exercise" in the score-card.html.
-
-const typeExercise=['Functional','Cycling','GYM','Running']
-
-function listExercise () {
-    for (let i=0; i < typeExercise.length; i++)
-    {console.log (typeExercise[i])}
-}
-
-// debugger
-
-class DayExercise {
-    constructor(exercise, roundRep, weight, time) {
-        this.exercise = exercise;
-        this.roundRep = roundRep;
-        this.weight = weight;
-        this.time = time;
+function logOut() {
+    let goOut = confirm("Are you sure you want to Log Out?")
+    if (goOut) {
+        location.href = "index.html"
     }
 }
 
-function newExercise() {
 
-    let exercise = prompt("Exercise");
-    let roundRep = prompt("Rounds, Reps");
-    let weight = prompt("Weight");
-    let time = prompt("Time, Rounds, Reps");
-    
-    rutine.push(new DayExercise(exercise, roundRep, weight, time))
+//Chronometer
 
-    console.table(rutine)
+document.addEventListener("DOMContentLoaded", () => {
+    const $timeElapsed = document.querySelector("#timeElapsed"),
+        $btnStart = document.querySelector("#btnStart"),
+        $btnPause = document.querySelector("#btnPause"),
+        $btnFlag = document.querySelector("#btnFlag"),
+        $btnReset = document.querySelector("#btnReset"),
+        $flagContainer = document.querySelector("#flagContainer");
+    let benchmarks = [],
+        idInterval,
+        initialTime = null;
+    let timeDiference = 0;
+
+    const hideItem = element => {
+        element.style.display = "none";
+    }
+
+    const showItem = element => {
+        element.style.display = "";
+    }
+
+    const addCero = value => {
+        if (value < 10) {
+            return "0" + value;
+        } else {
+            return "" + value;
+        }
+    }
+
+    const milsecToMinutes = (miliSec) => {
+        const minutes = parseInt(miliSec / 1000 / 60);
+        miliSec -= minutes * 60 * 1000;
+        seconds = (miliSec / 1000);
+        return `${addCero(minutes)}:${addCero(seconds.toFixed(1))}`;
+    };
+
+
+    const initiate = () => {
+        const justNow = new Date();
+        initialTime = new Date(justNow.getTime() - timeDiference);
+        clearInterval(idInterval);
+        idInterval = setInterval(refreshTime, 100);
+        hideItem($btnStart);
+        hideItem($btnReset);
+        showItem($btnFlag);
+        showItem($btnPause);
+    };
+    const pauseTime = () => {
+        timeDiference = new Date() - initialTime.getTime();
+        clearInterval(idInterval);
+        showItem($btnStart);
+        hideItem($btnFlag);
+        hideItem($btnPause);
+        showItem($btnReset);
+    };
+    const refreshTime = () => {
+        const justNow = new Date();
+        const diference = justNow.getTime() - initialTime.getTime();
+        $timeElapsed.textContent = milsecToMinutes(diference);
+    };
+    const flagTime = () => {
+        benchmarks.unshift(new Date() - initialTime.getTime());
+        putFlag();
+    };
+    const putFlag = () => {
+        $flagContainer.innerHTML = "";
+        for (const [indice, benchmark] of benchmarks.entries()) {
+            const $li = document.createElement("p");
+            $li.innerHTML = `<strong class="is-size-4">${benchmarks.length - indice}.</strong> ${milsecToMinutes(benchmark)}`;
+            $flagContainer.append($li);
+        }
+    };
+
+    const resetTime = () => {
+        if (!confirm("Do you want to reset the time?")) {
+            return;
+        }
+        clearInterval(idInterval);
+        init();
+        benchmarks = [];
+        putFlag();
+        timeDiference = 0;
+    }
+
+    const init = () => {
+        $timeElapsed.textContent = "00:00.0";
+        hideItem($btnPause);
+        hideItem($btnFlag);
+        hideItem($btnReset);
+    };
+    init();
+
+    $btnStart.onclick = initiate;
+    $btnFlag.onclick = flagTime;
+    $btnPause.onclick = pauseTime;
+    $btnReset.onclick = resetTime;
+});
+
+
+
+// Selector of "type of exercise" in the score-card.html.
+
+const typeSport = ['Functional', 'Cycling', 'GYM', 'Running']
+const selectSport = document.querySelector("#selectSport")
+
+function listSport() {
+    typeSport.sort();
+    typeSport.forEach(sport => {
+        selectSport.innerHTML += `<option>${sport}</option>`
+    })
 }
 
-// Array to enable save new DayExercise.
+listSport();
 
-const rutine = []
+
+
+// Button to add newExercise 
+const newExerciseForm = document.querySelector("#newExerciseForm")
+
+newExerciseForm.addEventListener("click", newExercise)
+
+
+function newExercise() {
+    let newForm = document.createElement("div");
+    newForm.innerHTML += `<div class="justify-content-center align-items-baseline row mt-4">
+    <div class="mb-2 text-center col-2">
+        <label for="name" class="form-label"></label>
+        <input type="text" name="name" class="form-control" id="name" placeholder="">
+    </div>
+    
+    <div class="mb-2 text-center col-2">
+        <label for="name" class="form-label"></label>
+        <input type="text" name="name" class="form-control" id="name" placeholder="">
+    </div>
+    
+    <div class="mb-2 text-center col-2">
+        <label for="name" class="form-label"></label>
+        <input type="text" name="name" class="form-control" id="name" placeholder="">
+    </div>
+    
+    <div class="mb-2 text-center col-2">
+        <label for="name" class="form-label"></label>
+        <input type="text" name="name" class="form-control" id="name" placeholder="">
+    </div>
+    
+    
+    </div>
+    <div class="d-flex justify-content-center">
+    <div class="form-floating col-5">
+        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"
+            style="height: 60px"></textarea>
+        <label for="floatingTextarea">Comments</label>
+    </div>
+    <div class="d-flex justify-content-end mt-5">
+    <button id="newExerciseForm" type="button" class="btn add-exercise-button">+</button>
+</div>
+    </div>`;
+    document.body.appendChild(newForm);
+}
+
+
+
+// //Save button 
+
+
+// let dateInput = document.querySelector("#dateInput");
+// let usernamInput = document.querySelector("#usernamInput");
+// let typeExerciseInput = document.querySelector("#typeExerciseInput");
+// //en el selector del tipo de ejercicio como hago par aponerl el ID?
+
+// let exerciseInput = document.querySelector("#exerciseInput");
+// let roundsInput = document.querySelector("#roundsInput");
+// let weightInput = document.querySelector("#weightInput");
+// let timeRoundInput = document.querySelector("#timeRoundInput");
+// let commentInput = document.querySelector("#commentInput");
+// //en el comment cómo hago par aponerl el ID?
+
+// const saveButton = document.querySelector("#saveButton")
+
+// saveButton.addEventListener("click", saveButton)
+
+// function saveButton(e) {
+//     e.preventDefault();
+
+//     let dateEntry = dateInput.value;
+//     let usernameEntry = usernamInput.value;
+//     let typeExerciseEntry = typeExerciseInput.value;
+//     let exerciseEntry = exerciseInput.value;
+//     let roundsEntry = roundsInput.value;
+//     let weightEntry = weightInput.value;
+//     let timeRoundEntry = timeRoundInput.value;
+//     let commentEntry = commentInput.value;
+
+//     rutine.push(saveButton(dateEntry, usernameEntry, typeExerciseEntry, exerciseEntry, roundsEntry, weightEntry, timeRoundEntry, commentEntry))
+
+//     console.table(rutine)
+// }
+
+// const rutine = []
+
+
+
+// // Clear button 
+
+// const clearButton = document.querySelector("#clearButton")
+
+// clearButton.addEventListener("click", clearButton)
+
+// function clearButton(e) {
+//     e.preventDefault();
+//     saveButton().remove()
+// }
+
 
